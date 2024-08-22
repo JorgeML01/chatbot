294ed9141df29a90a6710c96ecdb4da73fad6a6d
Jorge Matute <jorgedd2001@gmail.com>
2024-08-22 10:57:11 -0600

    login update fb button

--- chatbot-app/src/pages/LoginPage/LoginPage.jsx
+++ chatbot-app/src/pages/LoginPage/LoginPage.jsx
@@ -1,28 +1,48 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+
+// Facebook buttons
+import { LoginSocialFacebook } from "reactjs-social-login";
+import { FacebookLoginButton } from "react-social-login-buttons";
+
 import './styles.css';
 
 function LoginPage() {
   return (
     <div className="d-flex justify-content-center align-items-center login-body" style={{ height: '80vh' }}>
         <div className="w-50 form-style">
-        <h3>Log In</h3>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>User</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-            </Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Button type="submit" className="button-login">
-            Log in
-          </Button>
-      </Form>
+          <h3>Log In</h3>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>User</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button type="submit" className="button-login">
+              Log in
+            </Button>
+            <Button type="submit" className="button-login">
+              Reconociento facial
+            </Button>
+        </Form>
+        <LoginSocialFacebook
+          appId="1234585024333167"
+          onResolve={(response) => {
+            console.log(response);
+          }}
+          onReject={(error) => {
+            console.log(error);
+          }}
+        >
+          <FacebookLoginButton/>
+        </LoginSocialFacebook>
       </div>
+
     </div>
   );
 }
