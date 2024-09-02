af98c148b40dc15643f984fb5310e06ed400a935
Jorge Matute <jorgedd2001@gmail.com>
2024-09-02 14:37:54 -0600

    navbar update cookies

--- chatbot-app/src/components/Layout/NavbarLayout/NavbarLayout.jsx
+++ chatbot-app/src/components/Layout/NavbarLayout/NavbarLayout.jsx
@@ -5,33 +5,73 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import React, { useState, useEffect } from 'react';
 
 function NavbarLayout() {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function checkLoginStatus() {
+    const accessToken = Cookies.get("accessToken");
+    const refreshToken = Cookies.get("refreshToken");
+
+    if (accessToken && refreshToken) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }
+
+  function handleLogout() {
+    Cookies.remove("accessToken");
+    Cookies.remove("refreshToken");
+    navigate("/");
+    window.location.reload();
+  }
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
   return (
-    <>
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary navbar-body sticky-top">
-        <Container>
-          <Navbar.Brand href="/">SIL-CHATBOT</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="chatbot">Chat</Nav.Link> 
-            </Nav>
-            <Nav>
-              {/* Poner nombre y la fotito. Que se pueda agregar foto de perfil. Al darle que esté la opción de settings.*/}
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary navbar-body sticky-top">
+      <Container>
+        <Navbar.Brand href="/">SIL-CHATBOT</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            {isLoggedIn ? (
+              <>
+                <Nav.Link href="chatbot">Chat</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link href="/login">Log In</Nav.Link>
+                <Nav.Link href="/signup">Sign Up</Nav.Link>
+              </>
+            )}
+          </Nav>
+          <Nav>
+            {isLoggedIn ? (
               <NavDropdown title="Perfil" id="basic-nav-dropdown">
-              <NavDropdown.Item href="perfil">Perfil</NavDropdown.Item>
-              <NavDropdown.Item href="settings">
-                Settings
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/login">Log out</NavDropdown.Item>
-            </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+                <NavDropdown.Item href="perfil">Perfil</NavDropdown.Item>
+                <NavDropdown.Item href="settings">
+                  Settings
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={handleLogout}>
+                  Log out
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <></>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
