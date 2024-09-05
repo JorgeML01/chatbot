ef3082b854954dadb26fb694fa2f838bc5af7204
Jorge Matute <jorgedd2001@gmail.com>
2024-09-04 20:28:25 -0600

    unused vars removed

--- chatbot-app/src/pages/ChatbotPage/ChatbotPage.jsx
+++ chatbot-app/src/pages/ChatbotPage/ChatbotPage.jsx
@@ -1,25 +1,7 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
 import ChatbotComponent from '../../components/Chatbot';
 import './styles.css';  // Archivo CSS personalizado
 
 const Chatbot = () => {
-  const [messages, setMessages] = useState([]);
-  const [inputMessage, setInputMessage] = useState('');
-
-  const handleSendMessage = () => {
-    if (inputMessage.trim()) {
-      const userMessage = { text: inputMessage, sender: 'user' };
-      setMessages([...messages, userMessage]);
-      setInputMessage('');
-
-      // Simular respuesta del bot
-      setTimeout(() => {
-        const botMessage = { text: `Hola Buenos Días! Un gusto en conocerte. ¿Hay algo en lo que necesitas ayuda?`, sender: 'bot' };
-        setMessages(prevMessages => [...prevMessages, botMessage]);
-      }, 1000);
-    }
-  };
 
   return (
     <div className="">
